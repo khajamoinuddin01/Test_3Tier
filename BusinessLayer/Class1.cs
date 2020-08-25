ccd641ed1363603abd4166f406170ff08929d9bb
khajamoinuddin_er <khajamoinuddin_er@UKLDN966>
2020-08-25 13:57:01 +0100

    Add project files.

---
+++ BusinessLayer/Class1.cs
@@ -0,0 +1,28 @@
+﻿using IBusinessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer;
+using IBusinessLayer;
+using IDAL;
+
+namespace BusinessLayer
+{
+    public class TestBL : ITestBL
+    {
+         ITestDAL TestDAL;
+        #region Constructor
+        public TestBL()
+        {
+            TestDAL = new TestDAL();
+        }
+        #endregion Constructor
+
+        public bool TestMethod()
+        {
+            return true;
+        }
+    }
+}
---
+++ Test_3Tier/WebForm1.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using BusinessLayer;
+
+namespace Test_3Tier
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        TestBL testBL = new TestBL ();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            bool test = testBL.TestMethod();
+
+            TextBox1.Text = test.ToString();
+                 
+        }
+    }
+}
